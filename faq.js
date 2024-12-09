e091e38aea6a0690d010bafb0c01845104c9c660
Mariam-Hamza <168563916+Mariam-Hamza@users.noreply.github.com>
2024-12-09 19:40:50 +0200

    Update faq.js

--- faq.js
+++ faq.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const faqItems = document.querySelectorAll('.faq-question');
-
-    faqItems.forEach(item => {
-        item.addEventListener('click', function() {
-            const answer = this.nextElementSibling;
-            
-            // Toggle the visibility of the answer with smooth transition
-            if (answer.style.display === "block") {
-                answer.style.display = "none";
-            } else {
-                answer.style.display = "block";
-            }
-        });
-    });
-});
+msh 3rfa a delete this file ong
